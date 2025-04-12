9d6d6a790638c34fcc45108196f459cfe44b253a
kk-pechi <sara89bande89@gmail.com>
2025-04-12 21:31:06 +0900

    20章課題ディレクトリの追加

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+const changeBtn = document.getElementById('btn');
+
+changeBtn.addEventListener('click', () => {
+
+  const changeText = document.getElementById('text');
+
+  changeText.textContent = 'ボタンをクリックしました';
+
+});
